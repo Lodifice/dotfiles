8cc5d97b60ff0af932a4e4bedfbf7fe365d487d7
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2025-11-03 15:05:49 +0100

    nix-migration: add everything tracked except major 3
    Those being Bash, mutt, and Vim.

---
+++ TODO.md
@@ -0,0 +1,42 @@
+# TODO for nix migration
+
+- .bash_aliases
+- .bash_profile
+- .bashrc
+- .config/bash/history-exec.bash
+- .mutt/bindings
+- .mutt/colors.default
+- .mutt/colors.linux
+- .mutt/colors.transparent
+- .mutt/mailcap
+- .vim/after/ftplugin/mail.vim
+- .vim/after/ftplugin/tex.vim
+- .vim/after/syntax/tex.vim
+- .vim/pack/notetaking/start/markdown
+- .vim/pack/notetaking/start/zettelkasten
+- .vim/vimrc
+- bin/docs
+- bin/mailsync
+- bin/mutt_bgrun
+- bin/papers
+- bin/pinentry-switch
+
+# Xorg stuff that needs replacement
+
+- .XCompose
+- 00-keyboard.conf
+- my_gb
+
+# Xorg stuff to delete without replacement
+
+- .Xresources
+- .config/sxhkd/sxhkdrc
+- .config/urxvt/perl/resize-font
+- .config/urxvt/perl/vtwheel
+- .stalonetrayrc
+- st-font-and-colors.diff
+- st-pkgbuild.diff
+
+# Untracked stuff
+
+- gpgsm.conf and all keys
--- home.nix
+++ home.nix
@@ -27,6 +27,14 @@
     pkgs.mesa
     pkgs.libdrm
 
+    # packages with configuration files just copied by by Home Manager
+    # (at least for the time being)
+    pkgs.lf
+    pkgs.todoman
+
+    # fonts
+    pkgs.nerd-fonts.fantasque-sans-mono
+
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
@@ -56,6 +64,15 @@
     # '';
   };
 
+  # Config files just copied by Home Manager
+  xdg.configFile = {
+    "latexmk/latexmkrc".source = config/latexmk/latexmkrc;
+    "lf/lfrc".source = config/lf/lfrc;
+    "readline/inputrc".source = config/readline/inputrc;
+    "tmux/tmux.conf".source = config/tmux/tmux.conf;
+    "todoman/config.py".source = config/todoman/config.py;
+  };
+
   # Home Manager can also manage your environment variables through
   # 'home.sessionVariables'. These will be explicitly sourced when using a
   # shell provided by Home Manager. If you don't want to manage your shell
@@ -73,7 +90,13 @@
   #  /etc/profiles/per-user/richard/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    # EDITOR = "emacs";
+    VISUAL = "vim";
+    # some programs are stupid and don't respect $VISUAL precedence
+    EDITOR = "${config.home.sessionVariables.VISUAL}";
+    INPUTRC = "${config.home.homeDirectory}/.config/readline/inputrc";
+    PAPERSDIR = "${config.home.homeDirectory}/forschung/literatur";
+    PYTHON_BASIC_REPL = 1;
+    EXECIGNORE = "/usr/bin/latexminted";
   };
 
   # Let Home Manager install and manage itself.
@@ -136,8 +159,410 @@
       default-bg = "#FFFFFF";
     };
   };
+  programs.gpg = {
+    enable = true;
+  };
+  services.dunst = {
+    enable = true;
+    iconTheme = {
+      package = pkgs.adwaita-icon-theme;
+      name = "Adwaita";
+    };
+    settings = {
+      global = {
+        ### Display ###
+
+        # Which monitor should the notifications be displayed on.
+        monitor = 0;
+
+        # Display notification on focused monitor.  Possible modes are:
+        #   mouse: follow mouse pointer
+        #   keyboard: follow window with keyboard focus
+        #   none: don't follow anything
+        #
+        # "keyboard" needs a window manager that exports the
+        # _NET_ACTIVE_WINDOW property.
+        # This should be the case for almost all modern window managers.
+        #
+        # If this option is set to mouse or keyboard, the monitor option
+        # will be ignored.
+        follow = "none";
+
+        ### Geometry ###
+
+        # dynamic width from 0 to 300
+        # width = (0, 300)
+        # constant width of 300
+        width = "300";
+
+        # The maximum height of a single notification, excluding the frame.
+        # this was changed to behave like width in 1.12.0
+        height = "(0, 300)";
+
+        # Position the notification in the top right corner
+        origin = "top-right";
+
+        # Offset from the origin
+        offset = "(30, 20)";
+
+        # Scale factor. It is auto-detected if value is 0.
+        scale = 0;
+
+        # Maximum number of notification (0 means no limit)
+        notification_limit = 0;
+
+        ### Progress bar ###
+
+        # Turn on the progess bar. It appears when a progress hint is passed with
+        # for example dunstify -h int:value:12
+        progress_bar = true;
+
+        # Set the progress bar height. This includes the frame, so make sure
+        # it's at least twice as big as the frame width.
+        progress_bar_height = 10;
+
+        # Set the frame width of the progress bar
+        progress_bar_frame_width = 1;
+
+        # Set the minimum width for the progress bar
+        progress_bar_min_width = 150;
+
+        # Set the maximum width for the progress bar
+        progress_bar_max_width = 300;
+
+
+        # Show how many messages are currently hidden (because of
+        # notification_limit).
+        indicate_hidden = "yes";
+
+        # The transparency of the window.  Range: [0; 100].
+        # This option will only work if a compositing window manager is
+        # present (e.g. xcompmgr, compiz, etc.). (X11 only)
+        transparency = 0;
+
+        # Draw a line of "separator_height" pixel height between two
+        # notifications.
+        # Set to 0 to disable.
+        # If gap_size is greater than 0, this setting will be ignored.
+        separator_height = 2;
+
+        # Padding between text and separator.
+        padding = 8;
+
+        # Horizontal padding.
+        horizontal_padding = 8;
+
+        # Padding between text and icon.
+        text_icon_padding = 0;
+
+        # Defines width in pixels of frame around the notification window.
+        # Set to 0 to disable.
+        frame_width = 3;
+
+        # Defines color of the frame around the notification window.
+        frame_color = "#aaaaaa";
+
+        # Size of gap to display between notifications - requires a compositor.
+        # If value is greater than 0, separator_height will be ignored and a border
+        # of size frame_width will be drawn around each notification instead.
+        # Click events on gaps do not currently propagate to applications below.
+        gap_size = 0;
+
+        # Define a color for the separator.
+        # possible values are:
+        #  * auto: dunst tries to find a color fitting to the background;
+        #  * foreground: use the same color as the foreground;
+        #  * frame: use the same color as the frame;
+        #  * anything else will be interpreted as a X color.
+        separator_color = "frame";
+
+        # Sort messages by urgency.
+        sort = "yes";
+
+        # Don't remove messages, if the user is idle (no mouse or keyboard input)
+        # for longer than idle_threshold seconds.
+        # Set to 0 to disable.
+        # A client can set the 'transient' hint to bypass this. See the rules
+        # section for how to disable this if necessary
+        # idle_threshold = 120
+
+        ### Text ###
+
+        font = "Fantasque Sans Mono 10";
+
+        # The spacing between lines.  If the height is smaller than the
+        # font height, it will get raised to the font height.
+        line_height = 4;
+
+        # Possible values are:
+        # full: Allow a small subset of html markup in notifications:
+        #        <b>bold</b>
+        #        <i>italic</i>
+        #        <s>strikethrough</s>
+        #        <u>underline</u>
+        #
+        #        For a complete reference see
+        #        <https://docs.gtk.org/Pango/pango_markup.html>.
+        #
+        # strip: This setting is provided for compatibility with some broken
+        #        clients that send markup even though it's not enabled on the
+        #        server. Dunst will try to strip the markup but the parsing is
+        #        simplistic so using this option outside of matching rules for
+        #        specific applications *IS GREATLY DISCOURAGED*.
+        #
+        # no:    Disable markup parsing, incoming notifications will be treated as
+        #        plain text. Dunst will not advertise that it has the body-markup
+        #        capability if this is set as a global setting.
+        #
+        # It's important to note that markup inside the format option will be parsed
+        # regardless of what this is set to.
+        markup = "full";
+
+        # The format of the message.  Possible variables are:
+        #   %a  appname
+        #   %s  summary
+        #   %b  body
+        #   %i  iconname (including its path)
+        #   %I  iconname (without its path)
+        #   %p  progress value if set ([  0%] to [100%]) or nothing
+        #   %n  progress value if set without any extra characters
+        #   %%  Literal %
+        # Markup is allowed
+        format = "<b>%s</b>\\n%b";
+
+        # Alignment of message text.
+        # Possible values are "left", "center" and "right".
+        alignment = "left";
+
+        # Vertical alignment of message text and icon.
+        # Possible values are "top", "center" and "bottom".
+        vertical_alignment = "center";
+
+        # Show age of message if message is older than show_age_threshold
+        # seconds.
+        # Set to -1 to disable.
+        show_age_threshold = 60;
+
+        # Specify where to make an ellipsis in long lines.
+        # Possible values are "start", "middle" and "end".
+        ellipsize = "middle";
+
+        # Ignore newlines '\n' in notifications.
+        ignore_newline = "no";
+
+        # Stack together notifications with the same content
+        stack_duplicates = true;
+
+        # Hide the count of stacked notifications with the same content
+        hide_duplicate_count = false;
+
+        # Display indicators for URLs (U) and actions (A).
+        show_indicators = "yes";
+
+        ### Icons ###
+
+        # Recursive icon lookup. You can set a single theme, instead of having to
+        # define all lookup paths.
+        enable_recursive_icon_lookup = true;
+
+        # Set icon theme (only used for recursive icon lookup)
+        icon_theme = "Adwaita";
+        # You can also set multiple icon themes, with the leftmost one being used first.
+        # icon_theme = "Adwaita, breeze"
+
+        # Align icons left/right/top/off
+        icon_position = "left";
+
+        # Scale small icons up to this size, set to 0 to disable. Helpful
+        # for e.g. small files or high-dpi screens. In case of conflict,
+        # max_icon_size takes precedence over this.
+        min_icon_size = 32;
+
+        # Scale larger icons down to this size, set to 0 to disable
+        max_icon_size = 128;
+
+        # Paths to default icons (only neccesary when not using recursive icon lookup)
+        #icon_path = "/usr/share/icons/gnome/16x16/status/:/usr/share/icons/gnome/16x16/devices/";
+
+        ### History ###
+
+        # Should a notification popped up from history be sticky or timeout
+        # as if it would normally do.
+        sticky_history = "yes";
+
+        # Maximum amount of notifications kept in history
+        history_length = 20;
+
+        ### Misc/Advanced ###
+
+        # dmenu path.
+        dmenu = "/usr/bin/dmenu -p dunst:";
+
+        # Browser for opening urls in context menu.
+        browser = "/usr/bin/xdg-open";
+
+        # Always run rule-defined scripts, even if the notification is suppressed
+        always_run_script = true;
+
+        # Define the title of the windows spawned by dunst
+        title = "Dunst";
+
+        # Define the class of the windows spawned by dunst
+        class = "Dunst";
+
+        # Define the corner radius of the notification window
+        # in pixel size. If the radius is 0, you have no rounded
+        # corners.
+        # The radius will be automatically lowered if it exceeds half of the
+        # notification height to avoid clipping text and/or icons.
+        corner_radius = 3;
+
+        # Ignore the dbus closeNotification message.
+        # Useful to enforce the timeout set by dunst configuration. Without this
+        # parameter, an application may close the notification sent before the
+        # user defined timeout.
+        ignore_dbusclose = false;
+
+        ### Wayland ###
+        # These settings are Wayland-specific. They have no effect when using X11
+
+        # Uncomment this if you want to let notications appear under fullscreen
+        # applications (default: overlay)
+        # layer = top
+
+        # Set this to true to use X11 output on Wayland.
+        force_xwayland = false;
+
+        ### mouse
+
+        # Defines list of actions for each mouse event
+        # Possible values are:
+        # * none: Don't do anything.
+        # * do_action: Invoke the action determined by the action_name rule. If there is no
+        #              such action, open the context menu.
+        # * open_url: If the notification has exactly one url, open it. If there are multiple
+        #             ones, open the context menu.
+        # * close_current: Close current notification.
+        # * close_all: Close all notifications.
+        # * context: Open context menu for the notification.
+        # * context_all: Open context menu for all notifications.
+        # These values can be strung together for each mouse event, and
+        # will be executed in sequence.
+        mouse_left_click = "close_current";
+        mouse_middle_click = "do_action, close_current";
+        mouse_right_click = "close_all";
+
+        # Shortcuts are specified as [modifier+][modifier+]...key
+        # Available modifiers are "ctrl", "mod1" (the alt-key), "mod2",
+        # "mod3" and "mod4" (windows-key).
+        # Xev might be helpful to find names for keys.
+
+        # Close notification.
+        close = "ctrl+space";
+
+        # Close all notifications.
+        close_all = "ctrl+shift+space";
+
+        # Redisplay last message(s).
+        # On the US keyboard layout "grave" is normally above TAB and left
+        # of "1". Make sure this key actually exists on your keyboard layout,
+        # e.g. check output of 'xmodmap -pke'
+        history = "ctrl+grave";
+
+        # Context menu.
+        context = "ctrl+shift+period";
+      };
+
+      urgency_low = {
+        # IMPORTANT: colors have to be defined in quotation marks.
+        # Otherwise the "#" and following would be interpreted as a comment.
+        #background = "#222222"
+        background = "#A5C7FF";  # winter
+        foreground = "#888888";
+        timeout = 10;
+        # Icon for notifications with low urgency, uncomment to enable
+        #default_icon = /path/to/icon
+      };
+
+      urgency_normal = {
+        #background = "#285577"
+        background = "#000000C0";
+        frame_color = "#A5C7FF";  # winter
+        #frame_color = "#5DA9F6";  # winter
+        #frame_color = "#DCF88F"  # spring
+        foreground = "#ffffff";
+        timeout = 10;
+        # Icon for notifications with normal urgency, uncomment to enable
+        #default_icon = /path/to/icon
+      };
+
+      urgency_critical = {
+        background = "#900000";
+        foreground = "#ffffff";
+        frame_color = "#ff0000";
+        timeout = 0;
+        # Icon for notifications with critical urgency, uncomment to enable
+        #default_icon = /path/to/icon
+      };
+    };
+  };
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    # TODO let Home Manager manage pinentry
+    # https://nix-community.github.io/home-manager/options.xhtml#opt-services.gpg-agent.pinentry.package
+    extraConfig = ''
+      allow-loopback-pinentry
+      pinentry-program /home/richard/.local/bin/pinentry-switch
+      '';
+  };
   nixGL.packages = nixGL.packages;
   nixGL.defaultWrapper = "mesa";
   nixGL.installScripts = [ "mesa" ];
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      monospace = [ "Fantasque Sans Mono" ];
+    };
+    configFile.symbols = {
+      enable = true;
+      label = "nerd-font-symbols";
+      priority = 10;
+      text = ''
+        <?xml version="1.0"?>
+        <!DOCTYPE fontconfig SYSTEM "fonts.dtd">
+        <!--
+            fix classifying unknown fonts as sans serif
+            https://eev.ee/blog/2015/05/20/i-stared-into-the-fontconfig-and-the-fontconfig-stared-back-at-me/#trial-the-first-monospace-font-falling-back-to-proportional-unicode-glyph
+        -->
+        <fontconfig>
+            <alias>
+                <family>Fantasque Sans Mono</family>
+                <default><family>monospace</family></default>
+            </alias>
+            <alias>
+                <family>Iosevka</family>
+                <default><family>monospace</family></default>
+            </alias>
+            <alias>
+                <family>monospace</family>
+                <!--
+                <prefer><family>Iosevka</family></prefer>
+                -->
+                <prefer><family>Noto Sans Mono</family></prefer>
+            </alias>
+            <match>
+                <test compare="eq" name="family">
+                    <string>sans-serif</string>
+                </test>
+                <test compare="eq" name="family">
+                    <string>monospace</string>
+                </test>
+                <edit mode="delete" name="family"/>
+            </match>
+        </fontconfig>
+        '';
+    };
+  };
 }
 # vim: shiftwidth=2 softtabstop=2
