b78970675f11bd98ace6dea1bf18309a099c121d
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2025-11-13 22:32:52 +0100

    nix: add pass-otp

--- home.nix
+++ home.nix
@@ -489,6 +489,7 @@
   };
   programs.password-store = {
     enable = true;
+    package = pkgs.pass.withExtensions (exts: [ exts.pass-otp ]);
     settings = {
       PASSWORD_STORE_DIR = "${config.xdg.dataHome}/password-store";
     };
