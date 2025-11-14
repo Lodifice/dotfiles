56d068c09a6150f9a6e272e849cfbc36f0cc4d64
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2025-11-14 17:51:15 +0100

    dunst: styling, remove ignored options

--- home.nix
+++ home.nix
@@ -601,17 +601,6 @@
         # notification_limit).
         indicate_hidden = "yes";
 
-        # The transparency of the window.  Range: [0; 100].
-        # This option will only work if a compositing window manager is
-        # present (e.g. xcompmgr, compiz, etc.). (X11 only)
-        transparency = 0;
-
-        # Draw a line of "separator_height" pixel height between two
-        # notifications.
-        # Set to 0 to disable.
-        # If gap_size is greater than 0, this setting will be ignored.
-        separator_height = 2;
-
         # Padding between text and separator.
         padding = 8;
 
@@ -623,7 +612,7 @@
 
         # Defines width in pixels of frame around the notification window.
         # Set to 0 to disable.
-        frame_width = 3;
+        frame_width = 0;
 
         # Defines color of the frame around the notification window.
         frame_color = "#aaaaaa";
@@ -632,15 +621,7 @@
         # If value is greater than 0, separator_height will be ignored and a border
         # of size frame_width will be drawn around each notification instead.
         # Click events on gaps do not currently propagate to applications below.
-        gap_size = 0;
-
-        # Define a color for the separator.
-        # possible values are:
-        #  * auto: dunst tries to find a color fitting to the background;
-        #  * foreground: use the same color as the foreground;
-        #  * frame: use the same color as the frame;
-        #  * anything else will be interpreted as a X color.
-        separator_color = "frame";
+        gap_size = 6;
 
         # Sort messages by urgency.
         sort = "yes";
@@ -654,7 +635,7 @@
 
         ### Text ###
 
-        font = "Fantasque Sans Mono 10";
+        font = "FantasqueSansM Nerd Font Propo 12";
 
         # The spacing between lines.  If the height is smaller than the
         # font height, it will get raised to the font height.
@@ -776,12 +757,14 @@
         # Define the class of the windows spawned by dunst
         class = "Dunst";
 
+        layer = "notifications";
+
         # Define the corner radius of the notification window
         # in pixel size. If the radius is 0, you have no rounded
         # corners.
         # The radius will be automatically lowered if it exceeds half of the
         # notification height to avoid clipping text and/or icons.
-        corner_radius = 3;
+        corner_radius = 6;
 
         # Ignore the dbus closeNotification message.
         # Useful to enforce the timeout set by dunst configuration. Without this
@@ -842,20 +825,15 @@
       urgency_low = {
         # IMPORTANT: colors have to be defined in quotation marks.
         # Otherwise the "#" and following would be interpreted as a comment.
-        #background = "#222222"
-        background = "#A5C7FF";  # winter
-        foreground = "#888888";
+        background = "#A5ADCEFC";  # winter
+        foreground = "#181926";
         timeout = 10;
         # Icon for notifications with low urgency, uncomment to enable
         #default_icon = /path/to/icon
       };
 
       urgency_normal = {
-        #background = "#285577"
-        background = "#000000C0";
-        frame_color = "#A5C7FF";  # winter
-        #frame_color = "#5DA9F6";  # winter
-        #frame_color = "#DCF88F"  # spring
+        background = "#181926CC";
         foreground = "#ffffff";
         timeout = 10;
         # Icon for notifications with normal urgency, uncomment to enable
@@ -863,9 +841,8 @@
       };
 
       urgency_critical = {
-        background = "#900000";
+        background = "#E67172FC";
         foreground = "#ffffff";
-        frame_color = "#ff0000";
         timeout = 0;
         # Icon for notifications with critical urgency, uncomment to enable
         #default_icon = /path/to/icon
