eef6f2468db9a2115db24b9d37a62f75fac881ba
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2026-02-20 17:48:15 +0100

    nix: update to 25.11

--- flake.nix
+++ flake.nix
@@ -2,9 +2,9 @@
   description = "Nix dotfiles";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixGL = {
